a143fbccc2f5a38d5af1e13add38e76f7279678d
GianCarlooo14 <128136546+GianCarlooo14@users.noreply.github.com>
2023-03-30 02:08:56 +0800

    Update NFTMetacrafters.js

--- NFTMetacrafters.js
+++ NFTMetacrafters.js
@@ -1,13 +1,12 @@
 
 let NFTCollections = [];
 
-function mintNFT(eyes, gender, clothes, hands, background) {
+function mintNFT(eyes, gender, clothes, hands) {
     const aliens = {
         eyes: eyes,
         gender: gender,
         clothes: clothes,
-        hands: hands,
-        background: background
+        hands: hands
     }
     NFTCollections.push(aliens);
 }
@@ -23,9 +22,9 @@ function getTotalSupply () {
     console.log("TOTAL NFT COLLECTED: " + NFTCollections.length);
 }
 
-mintNFT(3, "Male", "Jacket", 2, "Black");
-mintNFT(4, "Female", "T-shirt", 10, "Purple");
-mintNFT(8, "Male", "Jersey Shirt", 4, "Yellow");
+mintNFT(3, "Male", "Jacket", 2);
+mintNFT(4, "Female", "T-shirt", 10);
+mintNFT(8, "Male", "Jersey Shirt", 4);
 
 listNFTs();
 
